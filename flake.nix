04ee034b8b1b45f0e397f038e0b4cbefeba1fcc5
nsaspy <nsaspy@airmail.cc>
2024-01-28 17:56:15 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+  outputs = { nixpkgs, flake-utils, ... }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      mousetrap = (with pkgs; stdenv.mkDerivation {
+          pname = "rss-cli";
+          version = "master";
+          src = fetchgit {
+            url = "https://github.com/Clortox/rss-cli";
+            rev = "e28bb8f66e941274b02cbc5ac5f28483bc26d588";
+            sha256 = "sha256-AWFkgIOrNjfdBAWa1EC5J26SP4EH3RfkXosRXpe3yPI=";
+          };
+          nativeBuildInputs = [
+            pkg-config
+          ];
+          buildInputs = [
+            curl.dev
+            rapidxml
+          ];
+          buildPhase = "make all -j $NIX_BUILD_CORES";
+          installPhase = ''
+          mkdir --parents "$out/bin"
+          cp bin/* $out/bin
+          '';
+        }
+      );
+    in rec {
+      defaultApp = flake-utils.lib.mkApp {
+        drv = defaultPackage;
+      };
+      defaultPackage = mousetrap;
+      devShell = pkgs.mkShell {
+        buildInputs = [
+          mousetrap
+          pkgs.libnotify
+        ];
+      };
+    }
+  );
+}
